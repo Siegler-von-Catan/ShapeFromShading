567add3a5dcb3b9f3dc020223587fca149a19204
XPerianer <dominik.meier@student.hpi.de>
2020-11-02 20:13:31 +0100

    Low level structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="ShapeFromShading",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.8',
+    install_requires=['shapefromshading'],
+    entry_points={
+            'console_scripts': [
+                'sealconvert3d = shapefromshading.main:main',
+            ],
+        },
+)
---
+++ shapefromshading/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from the shape of shading algorithm")
+
+
+if __name__ == "__main__":
+    main()
