1edc9cf251caa9cee1d6c5e8f51e0acacf1ba2fd
XPerianer <dominik.meier@student.hpi.de>
2020-11-08 14:49:21 +0100

    FIle for tsai mubarak

--- shapefromshading/main.py
+++ shapefromshading/main.py
@@ -1,6 +1,12 @@
+import cv2
+from shapefromshading.tsai_mubarak_linera import tsai_mubarak
+
 def main():
     print("Hello from the shape of shading algorithm")
-
+    img = cv2.imread('test.png')
+    result = tsai_mubarak(img)
+    cv2.imwrite('out.png', result)
+    print("Wrote out.png")
 
 if __name__ == "__main__":
     main()
---
+++ shapefromshading/tsai_mubarak_linera.py
@@ -0,0 +1,9 @@
+# This implements the Shape From Shading algorithm from the paper
+# Shape from shading using linear approximation
+# by Ping-Sing Tsai and Mubarak Shah (1994)
+
+import cv2
+import numpy as np
+
+def tsai_mubarak(image):
+    return image
