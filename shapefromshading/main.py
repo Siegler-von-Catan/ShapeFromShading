feb563bc6d6b385067b334c70fd167433d147d29
XPerianer <dominik.meier@student.hpi.de>
2020-12-06 14:58:02 +0100

    Some helpful command line options

--- shapefromshading/main.py
+++ shapefromshading/main.py
@@ -6,13 +6,14 @@ from shapefromshading.tsai_shah_linera import tsai_shah
 def main():
     parser = argparse.ArgumentParser(description='Seal Shape From Shading out of Potsdam')
     parser.add_argument('source')
+    parser.add_argument('-o', '--output', help="Filename to write to", default="out.png")
 
     args = parser.parse_args()
 
     img = cv2.imread(args.source)
     result = tsai_shah(img, -0.785, 1.4, 10)
-    cv2.imwrite('out.png', result)
-    print("Wrote out.png")
+    cv2.imwrite(args.output, result)
+    print("Wrote " + args.output)
 
 if __name__ == "__main__":
     main()
